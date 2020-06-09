c3ca69f6a5e09c762a722cdab022d79e59286b9f
osidekyle <hawkinskyle67@gmail.com>
2020-06-09 01:08:39 -0700

    selectors and sizing

---
+++ drawing.js
@@ -0,0 +1,11 @@
+window.addEventListener('load',()=>{
+    const canvas= document.querySelector('#canvas');
+    const context = canvas.getContext('2d');
+
+
+
+    //Resizing
+    canvas.height = window.innerHeight;
+    canvas.width= window.innerWidth;
+    
+});
